255535ae1190a37d7ab85c1fec141c7fd5aef8b5
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-16 08:04:18 +0000

    [Add]登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,45 @@
 class Admin::ItemsController < ApplicationController
+  # before_action :authenticate_admin!
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction,:price, :is_active, :item_image)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+ # belongs_to genre
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :price
+    validates :item_image
+  end
+  validates :is_active, inclusion:{in: [true, false]}
+  has_one_attached :item_imege
 end
