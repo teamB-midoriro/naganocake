12c313ef847672292dcdc2f35ac4b621ffa6696e
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-04-17 08:48:44 +0000

    customer新規登録エラー

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,14 +1,15 @@
 class Public::RegistrationsController < ApplicationController
-  before_action :configure_sign_up_params, if: :devise_controller?
+  before_action :configure_sign_up_params, only: [:create]
 
   def new
   end
 
   def create
-    @customer_sign_up = Customer.new
-    if @customer_sign_up.save
-      flash[:notice] = "Welcome! You have signed up successfully."
-      # redirect_to customers_my_page_path
+     @customer = Customer.new(configure_sign_up_params)
+    if @customer.save
+      redirect_to customers_my_page_path
+    else
+      render 'new'
     end
   end
 
@@ -17,10 +18,10 @@ class Public::RegistrationsController < ApplicationController
     customers_my_page_path
   end
 
-  protected
-  #sign_upのストロングパラメーター
+  private
+
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :password, :password_confirmation])
   end
 
 end
