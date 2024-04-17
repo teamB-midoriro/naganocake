1bd7b96c0c4af2a88517eaa797c6aba43e8aeadb
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-17 12:06:33 +0000

    [Update]カート一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,12 @@ class Item < ApplicationRecord
   end
   validates :is_active, inclusion:{in: [true, false]}
   has_one_attached :item_image
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
