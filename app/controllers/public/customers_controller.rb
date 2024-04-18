356ee9cb61145b186b204d375b0996313c2222b1
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-04-18 03:43:04 +0000

    [Add]マイページ編集機能まで

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,7 +1,31 @@
 class Public::CustomersController < ApplicationController
-#   before_action :authenticate_customer!
+  before_action :authenticate_customer!
 
   def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    customer = current_customer
+    if current_customer.update(customer_params)
+      redirect_to customers_my_page_path
+    end
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
--- app/helpers/admin/sessions_helper.rb
+++ app/helpers/admin/sessions_helper.rb
@@ -1,13 +1,2 @@
 module Admin::SessionsHelper
-  def resource_name
-    :admin
-  end
-
-  def resource
-   @resource ||= Admin.new
-  end
-
-  def devise_mapping
-   @devise_mapping ||= Devise.mappings[:admin]
-  end
 end
