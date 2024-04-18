1815bdcbe2eeb28aaa00dbd7c620fc2e65fe0118
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-18 03:29:23 +0000

    [Add]注文履歴はじめ

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,11 @@
 class Public::OrdersController < ApplicationController
+  # befor_action :athenticate_customer!
+  
+  def index
+    @orders = current_customer.orders
+  end
+  
+  def show
+    @order = currnet_customer.find(params[:id])
+  end
 end
