8a405ca91b1350720430360450d612019d05f2d9
fg0fg0fg <158440003+fg0fg0fg@users.noreply.github.com>
2024-04-18 12:19:28 +0000

    top add enum x

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,7 @@ class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @items = Item.page(params[:page]).per(10).order(created_at: :ASC)
+    @items = Item.all.page(params[:page]).per(10).order(created_at: :ASC)
 
   end
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,11 +3,11 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
-  enum manufacture_status:{
-    impossible_manufacture: 0,
-    waiting_manufacture: 1,
-    manufacturing: 2,
-    finish: 3
-  }
+  # enum manufacture_status:{
+  #   impossible_manufacture: 0,
+  #   waiting_manufacture: 1,
+  #   manufacturing: 2,
+  #   finish: 3
+  # }
   
 end
