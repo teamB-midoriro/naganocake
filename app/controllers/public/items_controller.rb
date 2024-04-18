364806bc00b823e92b75860212fb889f5574e17a
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-18 10:53:05 +0000

    [Add]カート機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,5 @@
 class Public::ItemsController < ApplicationController
-  # before_action :authenticate_customer!
+  before_action :authenticate_customer!
 
   def index
     @items = Item.all.page(params[:page]).per(8).order(created_at: :DESC)
@@ -8,6 +8,16 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    
+    if params[:cart_item] && params[:cart_item][:amount].present?
+      @cart_item.amount = params[:cart_item][:amount].to_i
+      if @cart_item.save
+        redirect_to cart_path, notice: "カートに追加しました"
+      else
+        flash.now[:alert] = "個数を選択してください"
+        render action: "show"
+      end
+    end
   end
 
   # def genre_search
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,7 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
+  validates :amount, presence: true
 
   # 小計を求めるメソッド
   def subtotal
