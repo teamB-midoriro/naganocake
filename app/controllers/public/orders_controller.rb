fc82319084fc9c73afa912ac5fede6eb5017d167
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-19 10:31:34 +0000

    [Add]注文履歴詳細

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,12 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
     @orders = current_customer.orders.all.page(params[:page]).per(8).order(created_at: :DESC)
   end
-  
+
   def show
-    @order = currnet_customer.find(params[:id])
+    @order = current_customer.find(params[:id])
+    @order_details = current_customer.order_details.all.page(params[:page]).per(8).order(created_at: :DESC)
   end
 end
