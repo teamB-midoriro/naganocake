378195ea0f8b9d9fbc86555c8c8587860f27bc70
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-19 08:30:59 +0000

    [Add]注文履歴一覧

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,7 @@ class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    @orders = current_customer.orders
+    @orders = current_customer.orders.all.page(params[:page]).per(8).order(created_at: :DESC)
   end
   
   def show
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  
+  def total_payment
+    item
+  end
 end
