4d11fa872b23166aaec4efa5d88bac2c5356db50
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-04-20 04:21:19 +0000

    [Add]制作ステータス途中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,3 +1,25 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
+  
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_detail.all
+    
+    is_active = true
+    if @order_detail.update(making_status_params)
+      # 制作ステータスが制作中の時は注文ステータスを制作中にする
+      @order.update(status: "making") if @order_detail.making_status == "manufacturing"
+        @order_details.each do |order_detail|
+          if order_detail.making_status != "manufacturing"
+            
+      end 
+      
+    end
+  end
+  
+  private
+  def making_status_params
+    params.require(:order_detail).permit(:making_status)
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,3 @@
 class OrderDetail < ApplicationRecord
+  enum making_status: {impossible_manufacture: 0, waiting_manufacture: 1, manufacturing: 2, finish: 3}
 end
