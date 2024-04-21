25172039cac14194c18b007fde1bb3188d946abf
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-04-21 02:28:23 +0000

    [Update]バリテーション/レイアウト設定

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,9 +10,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
-    customer = current_customer
-    if current_customer.update(customer_params)
-      redirect_to customers_my_page_path
+    @customer = current_customer
+    if @current_customer.update(customer_params)
+      redirect_to customers_my_page_path, notice: "ユーザー情報を更新しました"
+    else
+      render 'edit'
     end
   end
 
