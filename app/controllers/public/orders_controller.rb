a600eaf4ce7ba8f93bfed49a68affb02df9c5d75
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-04-22 09:02:46 +0000

    [Remove]order_controlle,viewの修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,24 +3,38 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @addresses = Address.all
   end
 
   def confirm
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @order = Order.new(order_params)
+    @address_type = params[:order][:address_type]
 
-    if params[:order][:address_type] == "0"  #自身の住所を選択
+    case @address_type
+    when "0"  #自身の住所を選択
+      @select_address = "〒" + current_customer.postal_code + "  " + current_customer.address
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
-    elsif params[:order][:address_type] == "1"  #登録済住所から選択
-      @address = Address.find(params[:order][:address_id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    else params[:order][:address_type] == "2"  #新しい住所を選択
-      @order.customer_id = current_customer.id
+    when "1"  #登録済住所から選択
+      unless params[:order][:address_id] == ""
+        @address = Address.find(params[:order][:address_id])
+        @select_address = "〒" + @address.postal_code + "  " + @address.address
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.name = @address.name
+      else
+        render 'new'
+      end
+    when "2"  #新しい住所を選択
+      unless params[:order][:new_postal_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
+        @select_address = "〒" + params[:order][:new_postal_code] + " " + params[:order][:new_address]
+        @order.postal_code = params[:order][:new_postal_code]
+        @order.address = params[:order][:new_address]
+        @order.name = params[:order][:new_name]
+      else
+        render 'new'
+      end
     end
 
     # 商品合計額の計算
@@ -40,14 +54,8 @@ class Public::OrdersController < ApplicationController
     @cart_items = current_customer.cart_items.all
 
     if @order.save
-      if @order.status == 0
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, amount: cart_item.amount, making_status: 0)
-        end
-      else
-        @cart_items.each do |cart_item|
-          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, amount: cart_item.amount, making_status: 1)
-        end
+      @cart_items.each do |cart_item|
+        OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.add_tax_price, amount: cart_item.amount, making_status: 0)
       end
       redirect_to orders_thanks_path
       @cart_items.destroy_all
